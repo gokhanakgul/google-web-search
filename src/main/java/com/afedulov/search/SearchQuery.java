8480f4a99adcebc6a66ff519a3770258f3b5a2f3
Alexander Fedulov <alexander.fedulov@gmail.com>
2015-07-23 17:34:32 +0200

    -cleanup

--- src/main/java/com/afedulov/search/SearchQuery.java
+++ src/main/java/com/afedulov/search/SearchQuery.java
@@ -16,18 +16,10 @@ public class SearchQuery {
   private final String  site;
   private final Integer numResults;
 
-  //TODO: to be implemented
-/*  private final Date    from;
-  private final Date    to;
-  private final Boolean verbatim;*/
-
   public SearchQuery(Builder builder) {
     this.query = builder.query;
     this.site = builder.site;
     this.numResults = builder.numResults;
-/*    this.from = builder.from;
-    this.to = builder.to;
-    this.verbatim = builder.verbatim;*/
   }
 
   public static class Builder {
@@ -35,9 +27,6 @@ public class SearchQuery {
     private final String  query;
     private       String  site;
     private       Integer     numResults = 10;
-/*    private       Date    from;
-    private       Date    to;
-    private       Boolean verbatim;*/
 
     public Builder(String query) {
       this.query = query;
@@ -56,22 +45,5 @@ public class SearchQuery {
     public SearchQuery build() {
       return new SearchQuery(this);
     }
-
-/*    public Builder from(Date from) {
-      this.from = from;
-      return this;
-    }
-
-    public Builder to(Date to) {
-      this.to = to;
-      return this;
-    }
-
-    public Builder verbatim(Boolean verbatim) {
-      this.verbatim = verbatim;
-      return this;
-    }*/
-
   }
-
 }
